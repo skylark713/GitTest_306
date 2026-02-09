594a4433e5045adf986cac5924129c81664c2546
Yejin Lee <wthb22923@gmail.com>
2026-02-09 16:57:30 +0900

    Update GoodsController.java

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -4,6 +4,8 @@ public class GoodsController {
 
 	public static void main(String[] args) {
 		int i =0;
+		public void test(){
+			System.out.println("원격에서 test추가함 - 시나리오 5번 테스트");
 	}
 
 	public void selectAll(){
