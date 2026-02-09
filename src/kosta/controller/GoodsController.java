5d961718f3e609358492c9995e3c986fcb99aaf2
Yejin Lee <wthb22923@gmail.com>
2026-02-09 17:38:02 +0900

    clone

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -3,13 +3,16 @@ package kosta.controller;
 public class GoodsController {
 
 	public static void main(String[] args) {
-		int i =0;
+		int i = 0;
+	}
+	
 		public void test(){
 			System.out.println("원격에서 test추가함 - 시나리오 5번 테스트");
-	}
+		}
 
-	public void selectAll(){
+		public void selectAll(){
+			System.out.println("곧 집에간다~~~");
 
-	}
+		}
 
 }
