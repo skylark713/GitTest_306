c30dcd7a3401d6ad6ad21ffafb7574b77670779b
Yejin Lee <wthb22923@gmail.com>
2026-02-09 16:04:38 +0900

    시나리오 3번 - GoodsService 수정

--- src/kosta/service/GoodsService.java
+++ src/kosta/service/GoodsService.java
@@ -1,5 +1,9 @@
 package kosta.service;
 
 public class GoodsService {
-
+	public void insert() {
+		System.out.println("insert call...");
 }
+	public void select() {
+		System.out.println("select 추가");
+	}
