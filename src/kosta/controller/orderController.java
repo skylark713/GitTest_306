0ff94e423fc9d8ae5378194c7613d8f015f4c0d3
Yejin Lee <wthb22923@gmail.com>
2026-02-10 12:14:38 +0900

    OrderController 수정

--- src/kosta/controller/orderController.java
+++ src/kosta/controller/orderController.java
@@ -4,5 +4,9 @@ public class orderController {
 	public void orderInsert() {
 		System.out.println("order 브랜치에서 추가함");
 	}
+	
+	public void orderCart() {
+		System.out.println("로컬 cart브랜치에서 추가함");
+	}
 
 }
