43d6ab1f0cbdff8296e414d126086168fc1b7f6c
Yejin Lee <wthb22923@gmail.com>
2026-02-10 12:22:31 +0900

    Update orderController.java

--- src/kosta/controller/orderController.java
+++ src/kosta/controller/orderController.java
@@ -5,8 +5,8 @@ public class orderController {
 		System.out.println("order 브랜치에서 추가함");
 	}
 	
-	public void orderCart() {
-		System.out.println("로컬 cart브랜치에서 추가함");
+	public void remoteCart() {
+		System.out.println("원격 cart브랜치에서 추가함");
 	}
 
 }
